a9c0691f1ec269bd7f13574ef25c75cef6bae46a
Ryan Routsong <routsongrm@nih.gov>
2025-04-03 11:04:11 -0400

    fix: opt not opts

--- workflow/scripts/seuratCiteSampleQC.R
+++ workflow/scripts/seuratCiteSampleQC.R
@@ -423,12 +423,12 @@ saveRDS(seur, 'seur_cluster.rds')
 if ("HTO" %in% Assays(seur)) {
     ifelse(!dir.exists(file.path(opt$workdir, "cite-seq-matrix")), dir.create(opt$workdir, "cite-seq-matrix")), FALSE)
     hto_mat <- GetAssayData(object = seur, assay = "HTO", slot = "data")
-    write.csv(hto_mat, file = file.path(opt$workdir, "cite-seq-matrix", paste0(opts$project, "_HTO_matrix.csv")))
+    write.csv(hto_mat, file = file.path(opt$workdir, "cite-seq-matrix", paste0(opt$project, "_HTO_matrix.csv")))
 }
 if ("ADT" %in% Assays(seur)) {
     ifelse(!dir.exists(file.path(opt$workdir, "cite-seq-matrix")), dir.create(opt$workdir, "cite-seq-matrix")), FALSE)
     adt_mat <- GetAssayData(object = seur, assay = "ADT", slot = "data")
-    write.csv(adt_mat, file = file.path(opt$workdir, "cite-seq-matrix", paste0(opts$project, "_ADT_matrix.csv")))
+    write.csv(adt_mat, file = file.path(opt$workdir, "cite-seq-matrix", paste0(opt$project, "_ADT_matrix.csv")))
 }
 
 writeLines(capture.output(devtools::session_info()), 'sessionInfo.txt')
