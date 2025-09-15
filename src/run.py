f201d60eba6ed3c3a668c955e7905220ad513202
Ryan Routsong <routsongrm@nih.gov>
2025-09-15 14:43:17 -0400

    fix: process sample names to cell ranger names on convertsion from input to samples

--- src/run.py
+++ src/run.py
@@ -505,7 +505,7 @@ def add_sample_metadata(input_files, config, group=None):
     config['samples'] = []
     for file in input_files:
         # Split sample name on file extension
-        sample = re.split('\.R[12]\.fastq\.gz', os.path.basename(file))[0]
+        sample = re.split('_[A-Za-z0-9]+\.R[12]\.fastq\.gz', os.path.basename(file))[0]
         if sample not in added:
             # Only add PE sample information once
             added.append(sample)
